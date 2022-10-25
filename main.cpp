4c1c549a65ee30365a3faa620a1d8562c588d1a5
Kamil Ruchaj <kamilruchaj@gmail.com>
2022-10-25 20:02:26 +0200

    Testing

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    // To do 1: Wihile loop
+    
+    
+    // To do 2: display a menu options - switch
+    
+    
+            // To do 3 : P - Print numbers
+            /* 
+             * If the user enters 'P' or 'p', you should display all of the elements (ints) in the list.
+                If the list is empty you should display "[] - the list is empty"
+                If the list is not empty then all the list element should be displayed inside square brackets separated by a space. 
+                For example, [ 1 2 3 4 5 ]
+                 * /
+                 * 
+            
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
